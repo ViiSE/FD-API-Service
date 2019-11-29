4447cc509186317d355aba0b89e738886de3873d
viise <viise@outlook.com>
2019-11-29 20:39:24 +1000

    Changed ProductsCreator

--- src/main/java/ru/fd/api/service/creator/ProductsCreator.java
+++ src/main/java/ru/fd/api/service/creator/ProductsCreator.java
@@ -1,7 +1,8 @@
 package ru.fd.api.service.creator;
 
 import ru.fd.api.service.entity.Products;
+import ru.fd.api.service.exception.ProductsCreatorException;
 
 public interface ProductsCreator {
-    Products create();
+    Products create() throws ProductsCreatorException;
 }
--- src/main/java/ru/fd/api/service/creator/ProductsCreatorDefaultImpl.java
+++ src/main/java/ru/fd/api/service/creator/ProductsCreatorDefaultImpl.java
@@ -3,7 +3,10 @@ package ru.fd.api.service.creator;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ru.fd.api.service.entity.Products;
+import ru.fd.api.service.exception.ProductsCreatorException;
+import ru.fd.api.service.exception.RepositoryException;
 import ru.fd.api.service.repository.ProductsRepository;
+import ru.fd.api.service.repository.processor.ProductsRepositoryProcessors;
 
 import java.util.List;
 
@@ -11,15 +14,25 @@ import java.util.List;
 @Scope("prototype")
 public class ProductsCreatorDefaultImpl implements ProductsCreator {
 
+    private final ProductsRepositoryProcessors prodsReposPrc;
     private final List<String> with;
 
-    public ProductsCreatorDefaultImpl(List<String> with) {
+    public ProductsCreatorDefaultImpl(ProductsRepositoryProcessors prodsReposPrc, List<String> with) {
+        this.prodsReposPrc = prodsReposPrc;
         this.with = with;
     }
 
     @Override
-    public Products create() {
-//        ProductsRepository repository = productsRepositoryProducer.getProductsRepositoryDefaultInstance(with);
-        return null;
+    public Products create() throws ProductsCreatorException {
+        try {
+            ProductsRepository prRepo = prodsReposPrc.processor("simple").apply(null);
+
+            for(String param: with)
+                prRepo = prodsReposPrc.processor(param).apply(prRepo);
+
+            return prRepo.readProducts();
+        } catch (RepositoryException ex) {
+            throw new ProductsCreatorException(ex.getMessage(), ex.getCause());
+        }
     }
 }
