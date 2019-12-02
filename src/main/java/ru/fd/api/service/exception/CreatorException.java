34dee03088a5c8d75e64882006acd0bf799a10ef
viise <viise@outlook.com>
2019-12-02 14:46:55 +1000

    Updated exceptions

--- src/main/java/ru/fd/api/service/exception/CreatorException.java
+++ src/main/java/ru/fd/api/service/exception/CreatorException.java
@@ -1,12 +1,12 @@
 package ru.fd.api.service.exception;
 
-public class ProductsCreatorException extends Exception {
+public class CreatorException extends Exception {
 
-    public ProductsCreatorException(String message) {
+    public CreatorException(String message) {
         super(message);
     }
 
-    public ProductsCreatorException(String message, Throwable cause) {
+    public CreatorException(String message, Throwable cause) {
         super(message, cause);
     }
 }
