9398889924c3ddb40a22bea8704231094a2f5ec3
viise <viise@outlook.com>
2019-12-02 14:42:31 +1000

    Added departments pojo

---
+++ src/main/java/ru/fd/api/service/data/DepartmentPojo.java
@@ -0,0 +1,24 @@
+package ru.fd.api.service.data;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class DepartmentPojo {
+
+    private final String id;
+    private final String name;
+
+    public DepartmentPojo(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/ru/fd/api/service/data/DepartmentsPojo.java
@@ -0,0 +1,23 @@
+package ru.fd.api.service.data;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class DepartmentsPojo {
+
+    private final List<DepartmentPojo> departments;
+
+    @JsonCreator
+    public DepartmentsPojo(@JsonProperty("departments") List<DepartmentPojo> departments) {
+        this.departments = departments;
+    }
+
+    public List<DepartmentPojo> getDepartments() {
+        return departments;
+    }
+}
