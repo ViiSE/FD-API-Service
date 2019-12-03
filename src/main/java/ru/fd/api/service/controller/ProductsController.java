9a76ed600e9a7df99f905dc3f2472208cbbb53ce
viise <viise@outlook.com>
2019-12-03 14:19:20 +1000

    Updated controller

--- src/main/java/ru/fd/api/service/controller/ProductsController.java
+++ src/main/java/ru/fd/api/service/controller/ProductsController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import ru.fd.api.service.creator.ProductsCreator;
-import ru.fd.api.service.data.ProductsPojo;
+import ru.fd.api.service.creator.*;
+import ru.fd.api.service.data.*;
 import ru.fd.api.service.exception.CreatorException;
 import ru.fd.api.service.log.LoggerServer;
-import ru.fd.api.service.producer.creator.ProductsCreatorProducer;
+import ru.fd.api.service.producer.creator.*;
+import ru.fd.api.service.producer.repository.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +19,19 @@ import java.util.List;
 public class ProductsController {
 
     @Autowired private ProductsCreatorProducer productsCrProducer;
-//    @Autowired private DepartmentsCreatorProducer departmentsCrProducer;
-//    @Autowired private StatusesCreatorProducer statusesCrProducer;
-//    @Autowired private AttributesCreatorProducer attributesCrProducer;
-//    @Autowired private AttributesGroupsCreatorProducer attributesGrCrProducer;
-//    @Autowired private UnitsCreatorProducer unitsCreatorProducer;
+    @Autowired private StatusesCreatorProducer statusesCrProducer;
+    @Autowired private StatusesRepositoryProducer statusesRepositoryProducer;
+    @Autowired private AttributesCreatorProducer attrCrProducer;
+    @Autowired private AttributesRepositoryProducer attrRepoProducer;
+    @Autowired private AttributeGroupsCreatorProducer attrGrCrProducer;
+    @Autowired private AttributeGroupsRepositoryProducer attrGrRepoProducer;
+    @Autowired private UnitsCreatorProducer unitsCrProducer;
+    @Autowired private UnitsRepositoryProducer unitsRepoProducer;
+    @Autowired private CategoriesCreatorProducer categoriesCrProducer;
+    @Autowired private CategoriesRepositoryProducer categoriesRepoProducer;
+//    @Autowired private BalancesCreatorProducer balancesCrProducer;
+//    @Autowired private BalancesRepositoryProducer balancesRepoProducer;
+
     @Autowired private LoggerServer logger;
 
     @GetMapping("/products")
@@ -39,73 +48,93 @@ public class ProductsController {
         }
     }
 
-//    @GetMapping("/products/statuses")
-//    @ResponseBody
-//    public StatusesPojo statuses() {
-//        try {
-//            StatusesCreator statusesCreator = statusesCrProducer.getStatusesCreatorDefaultInstance();
-//            StatusesPojo statusesPojo = (StatusesPojo) statusesCreator.create().formForSend();
-//            logger.info(ProductsController.class, "Site request statuses");
-//            return statusesPojo;
-//        } catch (ProductsCreatorException ex) {
-//            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
-//            return new StatusesPojo(new ArrayList<>());
-//        }
-//    }
+    @GetMapping("/products/statuses")
+    @ResponseBody
+    public StatusesPojo statuses() {
+        try {
+            StatusesCreator statusesCreator = statusesCrProducer.getStatusesCreatorDefaultInstance(
+                    statusesRepositoryProducer.getStatusesRepositoryDefaultInstance());
+            StatusesPojo statusesPojo = (StatusesPojo) statusesCreator.create().formForSend();
+            logger.info(ProductsController.class, "Site request statuses");
+            return statusesPojo;
+        } catch (CreatorException ex) {
+            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
+            return new StatusesPojo(new ArrayList<>());
+        }
+    }
 
-//    @GetMapping("/products/attributes")
-//    @ResponseBody
-//    public AttributesPojo attributes() {
-//        try {
-//            AttributesCreator attributesCreator = attributesCrProducer.getAttributesCreatorDefaultInstance();
-//            AttributesPojo attributesPojo = (AttributesPojo) attributesCreator.create().formForSend();
-//            logger.info(ProductsController.class, "Site request attributes");
-//            return attributesPojo;
-//        } catch (ProductsCreatorException ex) {
-//            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
-//            return new AttributesPojo(new ArrayList<>());
-//        }
-//    }
+    @GetMapping("/products/attributes")
+    @ResponseBody
+    public AttributesPojo attributes() {
+        try {
+            AttributesCreator attributesCreator = attrCrProducer.getAttributesCreatorDefaultInstance(
+                    attrRepoProducer.getAttributesRepositoryDefaultInstance());
+            AttributesPojo attributesPojo = (AttributesPojo) attributesCreator.create().formForSend();
+            logger.info(ProductsController.class, "Site request attributes");
+            return attributesPojo;
+        } catch (CreatorException ex) {
+            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
+            return new AttributesPojo(new ArrayList<>());
+        }
+    }
 
-//    @GetMapping("/products/attributes-groups")
-//    @ResponseBody
-//    public AttributesGroupsPojo attributesGroups() {
-//        try {
-//            AttributesGroupsCreator attributesGroupsCreator = attributesGrCrProducer.getAttributesGroupsCreatorDefaultInstance();
-//            AttributesGroupsPojo attributesGroupsPojo = (AttributesPojo) attributesGroupsCreator.create().formForSend();
-//            logger.info(ProductsController.class, "Site request attributes groups");
-//            return attributesGroupsPojo;
-//        } catch (ProductsCreatorException ex) {
-//            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
-//            return new AttributesGroupsPojo(new ArrayList<>());
-//        }
-//    }
+    @GetMapping("/products/attribute-groups")
+    @ResponseBody
+    public AttributeGroupsPojo attributesGroups() {
+        try {
+            AttributeGroupsCreator attributesGroupsCreator = attrGrCrProducer.getAttributeGroupsCreatorDefaultInstance(
+                    attrGrRepoProducer.getAttributeGroupsRepositoryDefaultInstance());
+            AttributeGroupsPojo attributesGroupsPojo = (AttributeGroupsPojo) attributesGroupsCreator.create().formForSend();
+            logger.info(ProductsController.class, "Site request attribute groups");
+            return attributesGroupsPojo;
+        } catch (CreatorException ex) {
+            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
+            return new AttributeGroupsPojo(new ArrayList<>());
+        }
+    }
 
-//    @GetMapping("/products/units")
-//    @ResponseBody
-//    public UnitsPojo units() {
-//        try {
-//            UnitsCreator unitsCreator = unitsCreatorProducer.getUnitsCreatorDefaultInstance();
-//            UnitsPojo unitsPojo = (UnitsPojo) unitsCreator.create().formForSend();
-//            logger.info(ProductsController.class, "Site request units");
-//            return unitsPojo;
-//        } catch (ProductsCreatorException ex) {
-//            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
-//            return new UnitsPojo(new ArrayList<>());
-//        }
-//    }
+    @GetMapping("/products/units")
+    @ResponseBody
+    public UnitsPojo units() {
+        try {
+            UnitsCreator unitsCreator = unitsCrProducer.getUnitsCreatorDefaultInstance(
+                    unitsRepoProducer.getUnitsRepositoryDefaultInstance());
+            UnitsPojo unitsPojo = (UnitsPojo) unitsCreator.create().formForSend();
+            logger.info(ProductsController.class, "Site request units");
+            return unitsPojo;
+        } catch (CreatorException ex) {
+            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
+            return new UnitsPojo(new ArrayList<>());
+        }
+    }
 
-//    @GetMapping("/products/categories")
+    @GetMapping("/products/categories")
+    @ResponseBody
+    public CategoriesPojo categories() {
+        try {
+            CategoriesCreator categoriesCreator = categoriesCrProducer.getCategoriesCreatorDefaultInstance(
+                    categoriesRepoProducer.getCategoriesRepositoryDefaultInstance());
+            CategoriesPojo categoriesPojo = (CategoriesPojo) categoriesCreator.create().formForSend();
+            logger.info(ProductsController.class, "Site request categories");
+            return categoriesPojo;
+        } catch (CreatorException ex) {
+            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
+            return new CategoriesPojo(new ArrayList<>());
+        }
+    }
+
+//    @GetMapping("/products/balances")
 //    @ResponseBody
-//    public CategoriesPojo categories() {
+//    public BalancesPojo balances() {
 //        try {
-//            CategoriesCreator categoriesCreator = categoriesCreatorProducer.getCategoriesCreatorDefaultInstance();
-//            CategoriesPojo categoriesPojo = (CategoriesPojo) categoriesCreator.create().formForSend();
-//            logger.info(ProductsController.class, "Site request categories");
-//            return categoriesPojo;
-//        } catch (ProductsCreatorException ex) {
+//            BalancesCreator balancesCreator = balancesCrProducer.getBalancesCreatorDefaultInstance(
+//                    balancesRepoProducer.getBalancesRepositoryDefaultInstance());
+//            BalancesPojo balancesPojo = (BalancesPojo) balancesCreator.create().formForSend();
+//            logger.info(ProductsController.class, "Site request balances");
+//            return balancesPojo;
+//        } catch (CreatorException ex) {
 //            logger.error(ProductsController.class, ex.getMessage() + "\n" + ex.getCause());
-//            return new CategoriesPojo(new ArrayList<>());
+//            return new BalancesPojo(new ArrayList<>());
 //        }
 //    }
 }
--- src/test/java/test/creator/StatusesCreator.java
+++
@@ -1,7 +0,0 @@
-package test.creator;
-
-import ru.fd.api.service.entity.Statuses;
-
-public interface StatusesCreator {
-    Statuses createStatuses();
-}
