67226ff880ce88472bc7c37332105be1b4be6a78
viise <viise@outlook.com>
2019-12-04 17:49:11 +1000

    Updated tests producer util

---
+++ src/test/java/ru/fd/api/service/producer/util/FDAPIServiceDirectoryProducerDefaultIntegrationTestNG.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.producer.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+import ru.fd.api.service.ApiServiceApplication;
+import ru.fd.api.service.util.FDAPIServiceCurrentDirectoryImpl;
+import ru.fd.api.service.util.FDAPIServiceDirectory;
+
+import static org.testng.Assert.assertTrue;
+import static test.message.TestMessage.testBegin;
+import static test.message.TestMessage.testEnd;
+
+@SpringBootTest(classes = ApiServiceApplication.class)
+public class FDAPIServiceDirectoryProducerDefaultIntegrationTestNG extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private FDAPIServiceDirectoryProducer fdapiServiceDirectoryProducer;
+
+    @Test
+    public void getFdAPIServiceCurrentDirectoryInstance() {
+        testBegin("FDAPIServiceDirectoryProducerDefault", "getFdAPIServiceCurrentDirectoryInstance()");
+
+        FDAPIServiceDirectory fdapiServiceDir = fdapiServiceDirectoryProducer.getFdAPIServiceCurrentDirectoryInstance();
+        assertTrue(fdapiServiceDir instanceof FDAPIServiceCurrentDirectoryImpl,
+                "FDAPIServiceDirectory: not a valid type!");
+        System.out.println("Instance: " + fdapiServiceDir);
+
+        testEnd("FDAPIServiceDirectoryProducerDefault", "getFdAPIServiceCurrentDirectoryInstance()");
+    }
+}
