5eedfb44928ede51206db826ab8b053dce937197
viise <viise@outlook.com>
2019-12-05 13:06:20 +1000

    Updated repository

--- src/main/java/ru/fd/api/service/repository/ProductsRepositorySimpleImpl.java
+++ src/main/java/ru/fd/api/service/repository/ProductsRepositorySimpleImpl.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.fd.api.service.entity.Products;
 import ru.fd.api.service.exception.RepositoryException;
+import ru.fd.api.service.producer.entity.ProductProducer;
 import ru.fd.api.service.repository.mapper.ProductsSimpleRowMapper;
 
 @Repository("productsRepositorySimple")
@@ -15,11 +16,25 @@ public class ProductsRepositorySimpleImpl implements ProductsRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private ProductProducer productProducer;
 
     @Override
     public Products readProducts() throws RepositoryException {
         try {
-            return jdbcTemplate.queryForObject("SQL HERE", new ProductsSimpleRowMapper());
+            return jdbcTemplate.queryForObject(
+                    "SELECT b.Name as tName, UUID_TO_CHAR(t.GID) as GIDTovar, coalesce(Left(dm.Ident, 11), '') as idCategory, coalesce(UUID_TO_CHAR(m.GID), '') as GIDMaker, coalesce(e.OKEI, 796) as OKEI,\n" +
+                    "       case when coalesce(t.ProcNDS, 20) = 0 or coalesce(t.ProcNDS, 20) = 18 then 20 else coalesce(t.ProcNDS, 20) end as TAX,\n" +
+                    "     t.Ident as tIdent\n" +
+                    "FROM Branch b\n" +
+                    "LEFT JOIN Tovar t on t.Kod = b.KodLeaf\n" +
+                    "LEFT JOIN DirectMainTovar dmt on dmt.Tovar = b.KodLeaf\n" +
+                    "LEFT JOIN DirectMain dm on dm.Kod = dmt.DirectMain\n" +
+                    "LEFT JOIN Maker m on m.Kod = t.Maker\n" +
+                    "LEFT JOIN Edism e on e.Name = t.Edism\n" +
+                    "WHERE b.KodTree = 1\n" +
+                    "AND b.Owner = (Select bb.Kod From Branch bb Where bb.KodTree = 1 and bb.Owner = 0 and bb.KodLeaf = -1 and bb.Name = 'Товары')\n" +
+                    "ORDER BY 1 Collate pxw_cyrl", new ProductsSimpleRowMapper(productProducer));
         } catch (DataAccessException ex) {
             throw new RepositoryException(ex.getMessage(), ex.getCause());
         }
--- src/main/java/ru/fd/api/service/repository/mapper/ProductsSimpleRowMapper.java
+++ src/main/java/ru/fd/api/service/repository/mapper/ProductsSimpleRowMapper.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 import ru.fd.api.service.entity.Product;
 import ru.fd.api.service.entity.Products;
 import ru.fd.api.service.entity.ProductsDefaultImpl;
+import ru.fd.api.service.producer.entity.ProductProducer;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,38 +14,35 @@ import java.util.List;
 
 public class ProductsSimpleRowMapper implements RowMapper<Products> {
 
-//    private final ProductProducer productProducer;
+    private final ProductProducer productProducer;
 
-//    public ProductsSimpleRowMapper(ProductProducer productProducer) {
-//        this.productProducer = productProducer;
-//    }
+    public ProductsSimpleRowMapper(ProductProducer productProducer) {
+        this.productProducer = productProducer;
+    }
 
     @Override
     public Products mapRow(ResultSet rs, int i) throws SQLException {
-//        List<Product> products = new ArrayList<>();
-//        while(rs.next()) {
-//            String id = rs.getString("TOVAR.GID");
-//            String categoryId = rs.getString("DIRECTMAIN.IDENT");
-//            String vendorId = rs.getString("MAKER.GID");
-//            String unitId = rs.getString("OKEI");
-//            String name = rs.getString("TOVAR.NAME");
-//            short tax = rs.getShort("TAX");
-//            String articul = "";
-//            String code = rs.getString("TOVAR.GID");
-//            boolean noReturn = rs.getBoolean("NO_RETURN");
-//
-//            products.add(productProducer.getProductProducerDefaultInstance(
-//                    id,
-//                    categoryId,
-//                    vendorId,
-//                    unitId,
-//                    name,
-//                    tax,
-//                    articul,
-//                    code,
-//                    noReturn));
-//        }
-//        return new ProductsDefaultImpl(products);
-        return null;
+        List<Product> products = new ArrayList<>();
+        while(rs.next()) {
+            String id = rs.getString("GIDTovar").trim();
+            String categoryId = rs.getString("idCategory").trim();
+            String vendorId = rs.getString("GIDMaker").trim();
+            String unitId = rs.getString("OKEI").trim();
+            String name = rs.getString("tName").trim().replaceFirst("\\. ", "");
+            short tax = rs.getShort("TAX");
+            String articul = "";
+            String code = rs.getString("tIdent").trim();
+
+            products.add(productProducer.getProductSimpleInstance(
+                    id,
+                    categoryId,
+                    vendorId,
+                    unitId,
+                    name,
+                    tax,
+                    articul,
+                    code));
+        }
+        return new ProductsDefaultImpl(products);
     }
 }
