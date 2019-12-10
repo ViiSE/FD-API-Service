befeef0ef2be27545496da1d37b84b61f78e6c7f
viise <viise@outlook.com>
2019-12-11 09:09:06 +1000

    Added database package

---
+++ src/main/java/ru/fd/api/service/database/SQLQueryCreatorFromFileStringImpl.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.database;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import ru.fd.api.service.exception.CreatorException;
+import ru.fd.api.service.exception.ReaderException;
+import ru.fd.api.service.producer.database.SQLQueryProducer;
+import ru.fd.api.service.producer.database.SQLReaderProducer;
+import ru.fd.api.service.util.FDAPIServiceDirectory;
+
+@Service("sqlQueryCreatorFromFileString")
+@Scope("prototype")
+public class SQLQueryCreatorFromFileStringImpl implements SQLQueryCreator<String, String> {
+
+    private final FDAPIServiceDirectory serviceDirectory;
+    private final SQLReaderProducer sqlReaderProducer;
+    private final SQLQueryProducer sqlQueryProducer;
+
+    public SQLQueryCreatorFromFileStringImpl(
+            FDAPIServiceDirectory serviceDirectory,
+            SQLReaderProducer sqlReaderProducer,
+            SQLQueryProducer sqlQueryProducer) {
+        this.serviceDirectory = serviceDirectory;
+        this.sqlReaderProducer = sqlReaderProducer;
+        this.sqlQueryProducer = sqlQueryProducer;
+    }
+
+    @Override
+    public SQLQuery<String> create(String source) throws CreatorException {
+        try {
+            return sqlReaderProducer.getSQLReaderFromFileStringInstance(serviceDirectory, sqlQueryProducer, source).read();
+        } catch (ReaderException ex) {
+            throw new CreatorException(ex.getMessage(), ex.getCause());
+        }
+    }
+}
---
+++ src/main/java/ru/fd/api/service/database/SQLReaderFromFileImpl.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.database;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import ru.fd.api.service.exception.ReaderException;
+import ru.fd.api.service.producer.database.SQLQueryProducer;
+import ru.fd.api.service.util.FDAPIServiceDirectory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+@Component("sqlReaderFromFile")
+@Scope("prototype")
+public class SQLReaderFromFileImpl implements SQLReader<String> {
+
+    private final FDAPIServiceDirectory serviceDirectory;
+    private final SQLQueryProducer sqlQueryProducer;
+    private final String filename;
+
+    public SQLReaderFromFileImpl(FDAPIServiceDirectory serviceDirectory, SQLQueryProducer sqlQueryProducer, String filename) {
+        this.serviceDirectory = serviceDirectory;
+        this.sqlQueryProducer = sqlQueryProducer;
+        this.filename = filename;
+    }
+
+    @Override
+    public SQLQuery<String> read() throws ReaderException {
+        StringBuilder content = new StringBuilder();
+
+        File file = new File(serviceDirectory.directory() + "sql" + File.separator + filename);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String st;
+            while ((st = br.readLine()) != null)
+                content.append(st).append("\n");
+        } catch (IOException ex) {
+            throw new ReaderException(ex.getMessage(), ex.getCause());
+        }
+
+        return sqlQueryProducer.getSQLQueryStringInstance(content.toString());
+    }
+}
