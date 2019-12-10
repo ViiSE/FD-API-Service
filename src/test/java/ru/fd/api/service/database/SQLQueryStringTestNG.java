511c9137e6b224fe157d2fd3eac1c798e621747a
viise <viise@outlook.com>
2019-12-11 09:16:45 +1000

    Added database

---
+++ src/test/java/ru/fd/api/service/database/SQLQueryStringTestNG.java
@@ -0,0 +1,60 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.database;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import ru.fd.api.service.ApiServiceApplication;
+import ru.fd.api.service.producer.database.SQLQueryProducer;
+
+import static org.testng.Assert.assertNotNull;
+import static test.message.TestMessage.testBegin;
+import static test.message.TestMessage.testEnd;
+
+@SpringBootTest(classes = ApiServiceApplication.class)
+public class SQLQueryStringTestNG extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private SQLQueryProducer sqlQueryProducer;
+
+    private SQLQuery<String> sqlQuery;
+
+    @BeforeClass
+    @Parameters({"content"})
+    public void setUpClass(String content) {
+        SQLQuery<String> sqlQuery = sqlQueryProducer.getSQLQueryStringInstance(content);
+        assertNotNull(sqlQuery, "SQLQuery is null!");
+    }
+
+
+    @Test
+    public void content() {
+        testBegin("SQLQueryString", "content()");
+
+        String content = sqlQuery.content();
+        assertNotNull(content, "Content is null!");
+        System.out.println("Content: " + content);
+
+        testEnd("SQLQueryString", "content()");
+    }
+
+}
---
+++ src/test/java/ru/fd/api/service/database/SQLReaderFromFileTestNG.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.database;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import ru.fd.api.service.ApiServiceApplication;
+import ru.fd.api.service.exception.ReaderException;
+import ru.fd.api.service.producer.database.SQLQueryProducer;
+import ru.fd.api.service.producer.database.SQLReaderProducer;
+import ru.fd.api.service.producer.util.FDAPIServiceDirectoryProducer;
+import ru.fd.api.service.util.FDAPIServiceDirectory;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+import static test.message.TestMessage.*;
+
+@SpringBootTest(classes = ApiServiceApplication.class)
+public class SQLReaderFromFileTestNG extends AbstractTestNGSpringContextTests {
+
+    @Autowired private FDAPIServiceDirectoryProducer fdapiServiceDirectoryProducer;
+    @Autowired private SQLReaderProducer sqlReaderProducer;
+    @Autowired private SQLQueryProducer sqlQueryProducer;
+
+    @BeforeClass
+    public void setUpClass() {
+        testBegin("SQLReaderFromFile");
+    }
+
+    private SQLReader createSQLReader(String filename) {
+        SQLReader sqlReader =
+                sqlReaderProducer.getSQLReaderFromFileStringInstance(
+                        fdapiServiceDirectoryProducer.getFdAPIServiceCurrentDirectoryInstance(),
+                        sqlQueryProducer,
+                        filename);
+        assertNotNull(sqlReader, "SQLReader is null!");
+        return sqlReader;
+    }
+
+    private void test(String fileName) {
+        try {
+            SQLQuery sqlQuery = createSQLReader(fileName).read();
+            assertNotNull(sqlQuery, "SQLQuery is null!");
+            String content = (String) sqlQuery.content();
+            assertFalse(content.isEmpty(), "SQLContent is empty!");
+            System.out.println(content);
+        } catch (ReaderException ex) {
+            catchMessage(ex);
+        }
+    }
+
+    @Test
+    @Parameters({"fileNameSuccess"})
+    public void read_success(String fileName) {
+        testMethod("read() [success]");
+        test(fileName);
+    }
+
+    @Test
+    @Parameters({"fileNameFileNotFound"})
+    public void read_fileIsNotFound(String fileName) {
+        testMethod("read() [fileIsNotFound]");
+        test(fileName);
+    }
+
+    @AfterClass
+    public void teardownClass() {
+        testEnd("SQLReaderFromFile");
+    }
+}
