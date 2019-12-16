dfc5bd31ef83f65dc194874a0e9deab0e3156302
viise <viise@outlook.com>
2019-12-16 14:33:53 +1000

    Added conf package and added swagger cong

---
+++ src/main/java/ru/fd/api/service/configuration/SwaggerConfiguration.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2019 ViiSE.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package ru.fd.api.service.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+@PropertySource("classpath:swagger.properties")
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ru.fd.api.service.controller"))
+                .paths(PathSelectors.any())
+                .build()
+//                .globalOperationParameters(operationParameters())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKeyAuthScheme()));
+    }
+
+//    private List<Parameter> operationParameters() {
+//        List<Parameter> headers = new ArrayList<>();
+//        headers.add(new ParameterBuilder().name("Authorization")
+//                .description("Необходим API токен. Форма Authorization: Bearer <токен>")
+//                .modelRef(new ModelRef("string")).parameterType("header")
+//                .defaultValue("Bearer <Введите токен здесь>")
+//                .required(true).build());
+//
+//        return headers;
+//    }
+
+    private static SecurityScheme apiKeyAuthScheme() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("Bearer",
+                authorizationScopes));
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder().scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("FD API")
+                .description("Добро пожаловать в документацию FD API")
+                .license("FD (c)")
+                .licenseUrl("")
+                .version("1.0")
+                .build();
+    }
+}
