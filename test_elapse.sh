204d60026cae33ae98171764c4c955878b1b746d
viise <viise@outlook.com>
2019-12-17 14:28:15 +1000

    Added tools: TestConclusion

--- test_elapse.sh
+++ test_elapse.sh
@@ -1,5 +1,10 @@
-for i in {1..10}
+input_file=$1
+output_file=$2
+for i in {1..20}
 do
 	mvn test -B
 	echo Test $i done 
 done
+echo Run TestConclusion...
+java -jar tools/Test_Conclusion.jar $input_file $output_file
+echo Done!
