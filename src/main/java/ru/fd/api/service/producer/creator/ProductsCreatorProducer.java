9cfce1eb3fe6b9218da39d06b66d2b65a9aac666
viise <viise@outlook.com>
2019-12-30 12:06:20 +1000

    updated service producer

--- src/main/java/ru/fd/api/service/producer/creator/ProductsCreatorProducer.java
+++ src/main/java/ru/fd/api/service/producer/creator/ProductsCreatorProducer.java
@@ -1,10 +1,12 @@
 package ru.fd.api.service.producer.creator;
 
 import ru.fd.api.service.creator.ProductsCreator;
+import ru.fd.api.service.data.OrderPojo;
 import ru.fd.api.service.repository.processor.ProductsRepositoryProcessors;
 
 import java.util.List;
 
 public interface ProductsCreatorProducer {
     ProductsCreator getProductsCreatorDefaultInstance(ProductsRepositoryProcessors prodsReposPrc, List<String> with);
+    ProductsCreator getProductsOrderCreatorInstance(OrderPojo orderPojo);
 }
--- src/main/java/ru/fd/api/service/producer/creator/ProductsCreatorProducerDefaultImpl.java
+++ src/main/java/ru/fd/api/service/producer/creator/ProductsCreatorProducerDefaultImpl.java
@@ -3,6 +3,7 @@ package ru.fd.api.service.producer.creator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import ru.fd.api.service.creator.ProductsCreator;
+import ru.fd.api.service.data.OrderPojo;
 import ru.fd.api.service.repository.processor.ProductsRepositoryProcessors;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class ProductsCreatorProducerDefaultImpl implements ProductsCreatorProduc
     @Override
     public ProductsCreator getProductsCreatorDefaultInstance(ProductsRepositoryProcessors prodsReposPrc, List<String> with) {
         return (ProductsCreator) ctx.getBean("productsCreatorDefault", prodsReposPrc, with);
+    }
+
+    @Override
+    public ProductsCreator getProductsOrderCreatorInstance(OrderPojo orderPojo) {
+        return (ProductsCreator) ctx.getBean("productsOrderCreator", orderPojo);
     }
 }
